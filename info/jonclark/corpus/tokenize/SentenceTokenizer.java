08492ea941bebf0a2049447fe6914c55deeabc0a
ardentlygnarly <>
2007-06-02 01:46:57 +0000

    Old version of a sentence tokenizer.

--- info/jonclark/corpus/tokenize/SentenceTokenizer.java
+++ info/jonclark/corpus/tokenize/SentenceTokenizer.java
@@ -7,39 +7,38 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 public class SentenceTokenizer {
-    
-    public SentenceTokenizer(Properties props) {
-	
-    }
-    
-    private boolean isSentenceBoundary(final String str) {
-	if(str.length() != 1) {
-	    return false;
-	} else {
-	    return (EnglishTokenizer.SENTENCE_PUNCTUATION.indexOf(str.charAt(0)) != -1);
+
+	public SentenceTokenizer(Properties props) {
+
 	}
-    }
-    
-    /**
-         * Returns sentences
-         * 
-         * @param text
-         * @return
-         */
-    public String[][] tokenizeToSentences(final Iterable<String> wordTokens) {
-	final ArrayList<String[]> allSentences = new ArrayList<String[]>(100);
-	final ArrayList<String> sentence = new ArrayList<String>(40);
-	final StringBuilder builder = new StringBuilder();
-	
-	for(final String token : wordTokens) {
-	    sentence.add(token);
-	    if(isSentenceBoundary(token)) {
-		String[] arrSentence = sentence.toArray(new String[sentence.size()]);
-		allSentences.add(arrSentence);
-		sentence.clear();
-	    }
+
+	private boolean isSentenceBoundary(final String str) {
+		if (str.length() != 1) {
+			return false;
+		} else {
+			return (EnglishTokenizer.SENTENCE_PUNCTUATION.indexOf(str.charAt(0)) != -1);
+		}
 	}
-	
-	return allSentences.toArray(new String[allSentences.size()][]);
-    }
+
+	/**
+	 * Returns sentences
+	 * 
+	 * @param text
+	 * @return
+	 */
+	public String[][] tokenizeToSentences(final String[] wordTokens) {
+		final ArrayList<String[]> allSentences = new ArrayList<String[]>(100);
+		final ArrayList<String> sentence = new ArrayList<String>(40);
+
+		for (final String token : wordTokens) {
+			sentence.add(token);
+			if (isSentenceBoundary(token)) {
+				String[] arrSentence = sentence.toArray(new String[sentence.size()]);
+				allSentences.add(arrSentence);
+				sentence.clear();
+			}
+		}
+
+		return allSentences.toArray(new String[allSentences.size()][]);
+	}
 }
